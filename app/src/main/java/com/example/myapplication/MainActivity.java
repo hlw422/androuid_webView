23f8289122ee39f86aa2efa54ec80a17d10fafa2
hlw422 <hlw422@126.com>
2024-05-13 22:14:14 +0800

    安卓webview 嵌入vue3服务

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.createWebView();
+    }
+
+    /* 创建 WebView 实例 */
+    @SuppressLint("SetJavaScriptEnabled")
+    private void createWebView() {
+        // 创建 WebView 实例并通过 id 绑定我们刚在布局中创建的 WebView 标签
+        // 这里的 R.id.webview 就是 activity_main.xml 中的 WebView 标签的 id
+        final WebView webView = (WebView) findViewById(R.id.webview);
+
+        // 设置 WebView 允许执行 JavaScript 脚本
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        // 确保跳转到另一个网页时仍然在当前 WebView 中显示
+        // 而不是调用浏览器打开
+       // webView.setWebViewClient(new WebViewClient());
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                 System.out.println(description);
+            }
+        });
+
+        // 加载指定网页
+        String url = "http://192.168.100.204:8066/";
+        webView.loadUrl(url);
+    }
+}
